3771f9de7f76baa6c46f73d36adb302e8d4a4212
goulartgusta <goulartgusta9@gmail.com>
2024-11-28 15:48:20 -0300

    adicionando lista de mangas para procura

--- src/main/java/br/com/almaviva/clone_conteudo_web/Main.java
+++ src/main/java/br/com/almaviva/clone_conteudo_web/Main.java
@@ -3,19 +3,30 @@ package br.com.almaviva.clone_conteudo_web;
 import java.util.Scanner;
 
 import br.com.almaviva.clone_conteudo_web.config.WebDriverConfig;
+import br.com.almaviva.clone_conteudo_web.repository.MangasRepository;
 import br.com.almaviva.clone_conteudo_web.service.ClonaConteudo;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//String[] mangas = MangasRepository.getMangasRepository();
-		//System.out.println("Olá, usuário! por favor, insira o nome do manga para buscar no site 'leitordemanga': ");
-		String url = "https://leitordemanga.com/ler-manga/dandadan/portugues-pt-br/capitulo-01/?style=list";
-		WebDriverConfig driver = new WebDriverConfig();
-		ClonaConteudo clonaConteudo = new ClonaConteudo(driver.getDriver(), url);
+		String[] mangas = MangasRepository.getMangasRepository();
+		System.out.println("Olá, usuário! por favor, insira o nome do manga para buscar no site 'leitordemanga': ");
+		String url;
 
-		clonaConteudo.clonarPagina();
-		driver.getDriver().quit();
+		for (String manga : mangas) {
+			url = sc.nextLine();
+			if (url.equalsIgnoreCase(manga)) {
+				System.out.println("Mangá válido! Clonando capítulos.");
+				url = "https://leitordemanga.com/ler-manga/" + url.replace(" ", "-").toLowerCase()
+						+ "/portugues-pt-br/capitulo-01/?style=list";
+				WebDriverConfig driver = new WebDriverConfig();
+				ClonaConteudo clonaConteudo = new ClonaConteudo(driver.getDriver(), url);
+
+				clonaConteudo.clonarPagina();
+				driver.getDriver().quit();
+			}
+		}
+
 		sc.close();
 	}
 }
